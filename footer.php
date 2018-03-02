2391b878710f74a43d4bb00d7b058b367595ccb5
Travis Wagner <travis@Travis-MBP.local>
2018-03-02 10:54:22 -0500

    footer template

--- footer.php
+++ footer.php
@@ -1,4 +1,14 @@
-<?php wp_footer(); ?>
+<section class="footer">
+ <div class="container">
+  <div class="footer__logo">
+    <img src="<?php echo get_template_directory_uri() . '/assets/img/solana-logo-color.png'; ?>" alt="">
+   </div>
+    <div class="footer__info"><a href="#" class="u-color-secondary">Privacy</a>&nbsp;&nbsp;|&nbsp;&nbsp;marketing@mobilityempowered.com&nbsp;&nbsp;|&nbsp;&nbsp;1-800-555-5555</div>
+    <div class="footer__info">© 2018 Mobility Empowered LLC&nbsp;&nbsp;|&nbsp;&nbsp;Website by Digital Marketing Agency Sparxoo</div>
+  </div>
+</section>
+
+ <?php wp_footer(); ?>
   </div>
 </body>
 </html>
