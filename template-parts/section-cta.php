f83f3ebb8d4d282fe4fceb517c5765a1ba9145ed
Travis Wagner <travis@Travis-MBP.fios-router.home>
2018-03-12 12:26:13 -0400

    cta sectin

---
+++ template-parts/section-cta.php
@@ -0,0 +1,6 @@
+<div class="cta">
+  <div class="cta__inner">
+    <h2 class="u-text-white">WANT TO KNOW MORE?</h2>
+    <a href="#" class="btn btn--large btn--primary">schedule a demo</a>
+  </div>
+</div>
