9c5692fde8d96f2e4f844ef33c47f870589b5cd8
Travis Wagner <travis@Travis-MBP.fios-router.home>
2018-03-12 13:18:44 -0400

    added fontawesome, moved to before stylesheet

--- functions/add-styles.php
+++ functions/add-styles.php
@@ -1,11 +1,17 @@
 <?php
 // add stylesheets
 function spx_styles() {
+
+  // google font
+  wp_enqueue_style( 'google-font-poppins', 'https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,700,700i' );
+
+  // font awesome icons
+  wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array(), '4.7.0' );
+
   // change version based on modified date
   $ver = filemtime( get_template_directory() . '/assets/css/main.css');
+  // main stylesheet
   wp_enqueue_style( 'main', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), $ver );
-
-  wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,700,700i' );
 }
 add_action( 'wp_enqueue_scripts', 'spx_styles' );
 ?>
