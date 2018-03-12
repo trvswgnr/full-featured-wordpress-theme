1ca6a5e4fff97b86be907d7086c7a9a3015db705
Travis Wagner <travis@Travis-MBP.fios-router.home>
2018-03-12 12:24:17 -0400

    solutions overview templatee

---
+++ templates/template-solutions-overview.php
@@ -0,0 +1,69 @@
+<?php /* Template Name: Solutions Overview */ ?>
+<?php get_header(); ?>
+
+<?php get_template_part('template-parts/section', 'page-hero'); ?>
+
+  <div class="section section--sm intro-text u-text-center">
+<div class="container">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet ut a, explicabo, ipsum tempora quas sunt dolores unde laudantium, soluta ullam illum itaque ab? Non adipisci, veritatis vero perspiciatis quasi!</p>
+  </div>
+</div>
+<!-- /.section -->
+
+<div class="solution-box">
+  <div class="solution-box__bg solution-box__bg--search" style="background-image: url('<?php echo get_template_directory_uri() . '/assets/img/bg-search.png'; ?>');"></div>
+  <div class="container">
+    <div class="row solution-box__row">
+      <div class="col-sm-6">
+        <div class="solution-box__content">
+          <h2>SEARCH</h2>
+          <hr class="hr--small hr--left hr--accent">
+          <h3>Open Marketplace Visibility</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi quam soluta earum mollitia vel labore molestiae. Consequatur laboriosam praesentium voluptatem optio sapiente, sit molestiae eius eligendi obcaecati distinctio, quaerat quae!</p>
+        </div>
+      </div>
+      <div class="col-sm-6"></div>
+    </div>
+  </div>
+</div>
+<!-- /.solution-box -->
+
+<div class="solution-box solution-box--reverse">
+  <div class="solution-box__bg solution-box__bg--select" style="background-image: url('<?php echo get_template_directory_uri() . '/assets/img/bg-select.png'; ?>');"></div>
+  <div class="container">
+    <div class="row solution-box__row">
+      <div class="col-sm-6">
+        <div class="solution-box__content">
+          <h2>Select</h2>
+          <hr class="hr--small hr--left hr--accent">
+          <h3>Open Marketplace Visibility</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi quam soluta earum mollitia vel labore molestiae. Consequatur laboriosam praesentium voluptatem optio sapiente, sit molestiae eius eligendi obcaecati distinctio, quaerat quae!</p>
+        </div>
+      </div>
+      <div class="col-sm-6"></div>
+    </div>
+  </div>
+</div>
+<!-- /.solution-box.solution-box--reverse -->
+
+<div class="solution-box">
+  <div class="solution-box__bg solution-box__bg--manage" style="background-image: url('<?php echo get_template_directory_uri() . '/assets/img/bg-manage.png'; ?>');"></div>
+  <div class="container">
+    <div class="row solution-box__row">
+      <div class="col-sm-6">
+        <div class="solution-box__content">
+          <h2>Manage</h2>
+          <hr class="hr--small hr--left hr--accent">
+          <h3>Open Marketplace Visibility</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi quam soluta earum mollitia vel labore molestiae. Consequatur laboriosam praesentium voluptatem optio sapiente, sit molestiae eius eligendi obcaecati distinctio, quaerat quae!</p>
+        </div>
+      </div>
+      <div class="col-sm-6"></div>
+    </div>
+  </div>
+</div>
+<!-- /.solution-box -->
+
+<?php get_template_part('template-parts/section','cta'); ?>
+
+<?php get_footer(); ?>
