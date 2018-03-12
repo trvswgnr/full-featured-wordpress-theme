161ffb95913bf6aacf1f7e83af7bfb422c28cca1
Travis Wagner <travis@Travis-MBP.fios-router.home>
2018-03-12 12:24:31 -0400

    page hero section template part

---
+++ template-parts/section-page-hero.php
@@ -0,0 +1,11 @@
+<div class="page-hero">
+  <div class="page-hero__bg"></div>
+  <div class="page-hero__lead">
+   <div class="page-hero__shape"></div>
+    <div class="page-hero__lead-inner">
+      <h2>SOME HERO TEXT</h2>
+      <hr class="hr--small hr--white">
+      <p>A Holistic Approach to Temporary Housing</p>
+    </div>
+  </div>
+</div>
