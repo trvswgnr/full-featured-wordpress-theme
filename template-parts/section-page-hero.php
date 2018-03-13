5db8f917c8fef0e66e584c85209fa51c07d36b75
Travis Wagner <travis@Travis-MBP.local>
2018-03-13 11:38:27 -0400

    changed hero to include page title

--- template-parts/section-page-hero.php
+++ template-parts/section-page-hero.php
@@ -3,7 +3,15 @@
   <div class="page-hero__lead">
    <div class="page-hero__shape"></div>
     <div class="page-hero__lead-inner">
-      <h2>SOME HERO TEXT</h2>
+     <?php
+  if ( have_posts() ) :
+    while ( have_posts() ) :
+      the_post();
+  ?>
+  <h2><?php the_title(); ?></h2>
+  <?php endwhile; else : ?>
+
+  <?php endif; ?>
       <hr class="hr--small hr--white">
       <p>A Holistic Approach to Temporary Housing</p>
     </div>
