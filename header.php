b35ea1cba94d96475ef1dcb7bc21a015e16a3856
Travis Wagner <travis@Travis-MBP.local>
2018-03-13 15:47:12 -0400

    mobile nav

--- header.php
+++ header.php
@@ -8,16 +8,16 @@
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-  <div class="page">
+  <div id="page">
     <header class="header">
       <div class="header__inner">
         <div class="container">
-          <div class="row flex-bottom header__row">
+          <div class="row flex-middle header__row">
             <div class="col-sm-4 col-xs-6">
-              <div class="logo"><img src="<?php echo get_template_directory_uri() . '/assets/img/solana-logo-color.png'; ?>" alt="Solana Empowered Logo" /></div>
+              <a href="<?php echo get_site_url(); ?>" class="logo"><img src="<?php echo get_template_directory_uri() . '/assets/img/solana-logo-color.png'; ?>" alt="Solana Empowered Logo" /></a>
             </div>
             <div class="col-sm-8 col-xs-6">
-              <div class="mobile-menu-toggle u-hidden-desktop u-text-right">
+              <div class="mobile-menu-toggle u-hidden-desktop u-text-right js-menu-toggle">
                 <img src="<?php echo get_template_directory_uri() . '/assets/img/menu-bars.svg'; ?>" alt="">
               </div>
                <div class="nav-wrapper u-visible-desktop">
@@ -37,6 +37,17 @@
         </div>
       </div>
     </header>
+
+    <div class="mobile-menu">
+      <div class="mobile-menu__close js-menu-toggle"></div>
+      <ul>
+        <li><a href="#">Features</a></li>
+        <li><a href="#">Suppliers</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Request Demo</a></li>
+        <li><a href="#"><span class="fa fa-lock fa-fw"></span>Login</a></li>
+      </ul>
+    </div>
 
       <?php
    /* wp_nav_menu( array(
