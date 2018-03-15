1ba33f1de79da9fbdb31879d5d621aacc05367ff
Travis Wagner <travis@Travis-MBP.local>
2018-03-15 11:12:21 -0400

    contact template created

---
+++ templates/template-contact.php
@@ -0,0 +1,11 @@
+<?php /* Template Name: Contact */ ?>
+
+<?php get_header(); ?>
+
+<?php get_template_part('template-parts/section', 'page-hero'); ?>
+
+
+
+<?php get_template_part('template-parts/section','cta'); ?>
+
+<?php get_footer(); ?>
