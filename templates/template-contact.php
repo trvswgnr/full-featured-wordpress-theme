ccc7021705f4625e3896a990c3c4253e51bf0790
Travis Wagner <trav@Traviss-MacBook-Pro.local>
2018-03-20 14:46:57 -0400

    added template parts

--- templates/template-contact.php
+++ templates/template-contact.php
@@ -4,7 +4,22 @@
 
 <?php get_template_part('template-parts/section', 'page-hero'); ?>
 
+<div class="container form-wrapper">
+  <?php get_template_part('template-parts/loop', 'content'); ?>
+</div>
 
+<section class="section" id="Contact_our_location">
+  <div class="container u-text-center">
+    <h2>OUR LOCATION</h2>
+    <hr class="hr--small hr--accent">
+    <address class="address-contact">
+      1604 E 4th Ave<br>
+      Tampa, FL 33605<br>
+      1-833-273-5663<br>
+      <a href="mailto:marketing@mobilityempowered.com">marketing@mobilityempowered.com</a>
+    </address>
+  </div>
+</section>
 
 <?php get_template_part('template-parts/section','cta'); ?>
 
