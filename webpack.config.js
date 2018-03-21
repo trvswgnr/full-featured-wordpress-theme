d7294b623c38f23126f0adea2b59664b3fd05174
Travis Wagner <trav@Traviss-MacBook-Pro.local>
2018-03-21 16:59:17 -0400

    format

--- webpack.config.js
+++ webpack.config.js
@@ -37,9 +37,7 @@ module.exports = {
       host: 'localhost',
       port: 3000,
       proxy: 'http://localhost/solana-empowered',
-      files: [
-            '**/*.php'
-        ]
+      files: ['**/*.php']
     })
   ]
 };
