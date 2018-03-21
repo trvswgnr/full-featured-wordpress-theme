113966411645a289f3d1e24330e15fadf4494c9b
Travis Wagner <trav@Traviss-MacBook-Pro.local>
2018-03-21 17:01:53 -0400

    mobile niceties

--- footer.php
+++ footer.php
@@ -3,8 +3,8 @@
   <div class="footer__logo">
     <img src="<?php echo get_template_directory_uri() . '/assets/img/solana-logo-color.png'; ?>" alt="">
    </div>
-    <div class="footer__info"><a href="#" class="u-color-secondary">Privacy</a>&nbsp;&nbsp;|&nbsp;&nbsp;marketing@mobilityempowered.com&nbsp;&nbsp;|&nbsp;&nbsp;1-800-555-5555</div>
-    <div class="footer__info">© 2018 Mobility Empowered LLC&nbsp;&nbsp;|&nbsp;&nbsp;Website by Digital Marketing Agency Sparxoo</div>
+    <div class="footer__info"><span><a href="#" class="u-color-secondary">Privacy</a></span><span class="u-hidden-sm">&nbsp;&nbsp;|&nbsp;&nbsp;</span><span>marketing@mobilityempowered.com</span><span class="u-hidden-sm">&nbsp;&nbsp;|&nbsp;&nbsp;</span><span>1-800-555-5555</span></div>
+    <div class="footer__info"><span>© 2018 Mobility Empowered LLC</span><span class="u-hidden-sm">&nbsp;&nbsp;|&nbsp;&nbsp;</span><span class="sparxoo-credit">Website by Digital Marketing Agency <a href="http://sparxoo.com">Sparxoo</a></span></div>
   </div>
 </section>
 
