3f9299ebfc48e1c543c06181598fb943f24ecbb1
Travis Wagner <trav@Traviss-MacBook-Pro.local>
2018-03-21 13:18:10 -0400

    fit text to container on mobile

---
+++ src/js/fit-text.js
@@ -0,0 +1,24 @@
+function fitText(item) {
+  item.style.display = 'inline-block';
+  item.style.fontSize = '1px';
+  var parentWidth = item.parentNode.offsetWidth;
+  var percentage = parentWidth / item.offsetWidth;
+  var size = 0;
+  while (item.offsetWidth < parentWidth) {
+    size += 1;
+    item.style.fontSize = size + 'px';
+  }
+  item.style.fontSize = size - 1 + 'px';
+}
+
+var elements = document.querySelectorAll('.js-fit-text');
+
+window.onresize = function () {
+  let window_width = $(window).width();
+  console.log(window_width);
+  if (window_width <= 460) {
+  Array.prototype.forEach.call(elements, fitText);
+  }
+};
+
+window.onresize();
