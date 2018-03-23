b9df7f81d7c6bd3f14a71cf8929a127e0c26c4ac
Travis Wagner <trav@Traviss-MacBook-Pro.local>
2018-03-23 13:50:13 -0400

    qa changes

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
   <div class="footer__logo">
     <img src="<?php echo get_template_directory_uri() . '/assets/img/solana-logo-color.png'; ?>" alt="">
    </div>
-    <div class="footer__info"><span><a href="#" class="u-color-secondary">Privacy</a></span><span class="u-hidden-sm">&nbsp;&nbsp;|&nbsp;&nbsp;</span><span>marketing@mobilityempowered.com</span><span class="u-hidden-sm">&nbsp;&nbsp;|&nbsp;&nbsp;</span><span>1-800-555-5555</span></div>
+    <div class="footer__info"><span><a href="#" class="u-color-secondary">Privacy</a></span><span class="u-hidden-sm">&nbsp;&nbsp;|&nbsp;&nbsp;</span><span><a href="mailto:marketing@mobilityempowered.com"></a>Email Us</span><span class="u-hidden-sm">&nbsp;&nbsp;|&nbsp;&nbsp;</span><span><a href="tel:18332735663">1-833-273-5663</a></span></div>
     <div class="footer__info"><span>© 2018 Mobility Empowered LLC</span><span class="u-hidden-sm">&nbsp;&nbsp;|&nbsp;&nbsp;</span><span class="sparxoo-credit">Website by Digital Marketing Agency <a href="http://sparxoo.com">Sparxoo</a></span></div>
   </div>
 </section>
