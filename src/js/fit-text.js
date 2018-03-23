554825a49de506627d4d535adc7f563864666d15
Travis Wagner <trav@Traviss-MacBook-Pro.local>
2018-03-23 13:34:58 -0400

    better

--- src/js/fit-text.js
+++ src/js/fit-text.js
@@ -1,23 +1,31 @@
-function fitText(item) {
-  item.style.display = 'inline-block';
-  item.style.fontSize = '1px';
-  var parentWidth = item.parentNode.offsetWidth;
-  var percentage = parentWidth / item.offsetWidth;
-  var size = 0;
-  while (item.offsetWidth < parentWidth) {
-    size += 1;
-    item.style.fontSize = size + 'px';
+import { VIEWPORT_WIDTH } from './globals';
+
+export const fitText = () => {
+  let elements;
+
+  function calcSize(item) {
+
+    let parentWidth,
+        percentage,
+        size;
+
+    item.style.display  = 'inline-block';
+    item.style.fontSize = '1px';
+    parentWidth         = item.parentNode.offsetWidth;
+    percentage          = parentWidth / item.offsetWidth;
+    size                = 0;
+
+    while (item.offsetWidth < parentWidth) {
+      size += 1;
+      item.style.fontSize = size + 'px';
+    }
+
+    item.style.fontSize = size - 1 + 'px';
   }
-  item.style.fontSize = size - 1 + 'px';
-}
 
-var elements = document.querySelectorAll('.js-fit-text');
+  elements = document.querySelectorAll('.js-fit-text');
 
-window.onresize = function () {
-  let window_width = $(window).width();
-  if (window_width <= 460) {
-  Array.prototype.forEach.call(elements, fitText);
+  if (VIEWPORT_WIDTH <= 460) {
+    Array.prototype.forEach.call(elements, calcSize);
   }
 };
-
-window.onresize();
