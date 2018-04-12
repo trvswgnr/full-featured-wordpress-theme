79ccbcc7d7a1c0d4e334c2c94d98b9cf8cb9a5ea
Travis Wagner <trav@Traviss-MacBook-Pro.local>
2018-04-12 12:27:56 -0400

    'diversity' section acf image

--- templates/template-about.php
+++ templates/template-about.php
@@ -4,6 +4,7 @@
 
 <?php get_template_part('template-parts/section', 'page-hero'); ?>
 
+
 <?php $module_1 = get_field('about_module_1'); ?>
 
 <section class="section" id="our_story">
@@ -19,9 +20,10 @@
   </div>
 </section>
 
+
 <?php $module_2 = get_field('about_module_2'); ?>
 
-<section class="section section--tertiary has-image">
+<section class="section section--tertiary has-image" id="mission">
 <div class="section__bg" style="background-image: url('<?php echo get_template_directory_uri() . '/assets/img/about-image-bg-1.png'; ?>')"></div>
  <div class="container u-text-center">
   <h2 class="u-text-center u-color-white"><?php echo $module_2['heading']; ?></h2>
@@ -32,9 +34,11 @@
   </div>
 </section>
 
+
 <?php $module_3 = get_field('about_module_3'); ?>
+
 <section id="diversity" class="section section--image-left">
- <div class="section__img" style="background-image: url('<?php echo get_template_directory_uri() . '/assets/img/about-diversity.png'; ?>')"></div>
+ <div class="section__img" style="background-image: url('<?php echo $module_3['image']['url']; ?>')"></div>
   <div class="row">
     <div class="col-sm-6"></div>
     <div class="col-sm-5">
@@ -50,24 +54,26 @@
 </section>
 <!-- /#diversity -->
 
+
 <?php $module_4 = get_field('about_module_4'); ?>
+
 <div class="container">
  <h2 class="u-text-center"><?php echo $module_4['heading']; ?></h2>
   <hr class="hr--small hr--accent">
   <div class="carousel">
-<?php
-$member_args = array(
-  'post_type'   => 'teammember',
-  'post_status' => 'publish'
-);
-$members = new WP_Query( $member_args );
-if( $members->have_posts() ) :
-  while( $members->have_posts() ) :
-    $members->the_post();
-    $member_name = get_the_title();
-    $member_description = get_the_content();
-    $member_title = get_field('member_title');
-?>
+  <?php
+  $member_args = array(
+    'post_type'   => 'teammember',
+    'post_status' => 'publish'
+  );
+  $members = new WP_Query( $member_args );
+  if( $members->have_posts() ) :
+    while( $members->have_posts() ) :
+      $members->the_post();
+      $member_name = get_the_title();
+      $member_description = get_the_content();
+      $member_title = get_field('member_title');
+  ?>
     <div class="carousel__slide">
       <div class="team-member">
         <div class="row">
